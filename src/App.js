4079180cc2e312f6884dc3ecab0531b545e78243
RhyanSPisoni <rhyanoficial14@gmail.com>
2023-11-22 16:34:19 -0300

    Att

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-
-import HeaderMain from './utils/HeaderMain/HeaderMain'
+import HeaderMain from './utils/HeaderMain/HeaderMain';
 
 function App() {
-  const imgCenter = 'https://via.placeholder.com/1000x150';
+  const imgCenter = 'https://via.placeholder.com/154';
 
   return (
     <div className="App">
       <HeaderMain />
-      <div className='BodyCenter'>
-        <p>Teste</p>
+      <div className='logo-center'>
+        <img src={imgCenter} alt={imgCenter} />
       </div>
     </div>
   );
--- src/utils/HeaderMain/HeaderMain.js
+++ src/utils/HeaderMain/HeaderMain.js
@@ -6,19 +6,34 @@ function HeaderMain() {
         <div>
             <div class='container'>
                 <header>
-                    <div>
-                        <span>Home</span>
-                        <span>Projetos</span>
-                        <span>Contato</span>
+                    <div className='container-left'>
+                        <span>HOME</span>
+                        <span onClick={() => TestLinkUrl(2)}>PROJETOS</span>
+                        <span onClick={() => TestLinkUrl(1)} >CONTATO</span>
                     </div>
-                    <div>
+                    <div className='container-right'>
                         <span>LOGFAST DEV</span>
-                        <img src={imgLogin} />
+                        <img src={imgLogin} alt={imgLogin} />
                     </div>
                 </header >
             </div>
         </div>
     );
+
+    function TestLinkUrl(number) {
+        switch (number) {
+            case 1:
+                window.location.href = "https://www.linkedin.com/in/rhyan-de-souza-pisoni";
+                break;
+            case 2:
+                window.location.href = "https://github.com/rhyanspisoni";
+                break;
+            default:
+                break;
+        }
+
+    }
 }
+
 
 export default HeaderMain;
